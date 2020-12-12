00095512b28e0248f0c5901d736ea877109f26f9
mabasasi <aoe317293@yahoo.co.jp>
2020-12-12 11:13:32 +0900

    [add] cli の remove と list, default を作成
    ・package.json の commit 忘れてたかも…
    ・add cac
    ・add delay

--- src/index.ts
+++ src/index.ts
@@ -21,20 +21,39 @@ cli
     CliLog.info('[Command] Remove channel')
 
     for (const channelId of channelIds) {
-      const channel = await Channel.findOne({ channelId: channelId })
+      const query = !isNaN(channelId)
+        ? { id: channelId }
+        : { channelId: channelId }
+      const channel = await Channel.findOne(query)
       if (channel) {
         channel.setDeleteFlag()
         await channel.save()
-        CliLog.debug(`> [${channelId}] remove`)
+        CliLog.debug(`> [${channelId}] remove ${channel.title}`)
       }
     }
   })
 
 cli
-  .command('', 'Run')
-  .action(async () => {
-    CliLog.info('[Command] Run')
+.command('channel:list', 'Show list of channel')
+.action(async (options) => {
+  CliLog.info('[Command] Show list of channel')
 
+  const channels = await Channel.find()
+  channels.forEach((c) => {
+    const del = Boolean(c.deletedAt) ? '(delete) ' : ''
+    const text = `[${c.id}] ${del}${c.channelId} ${c.title}`
+    console.log(text)
+  })
+})
+
+cli
+  .command('', 'Run')
+  .action(async (options) => {
+    if (cli.args.length > 0) {
+      throw new Error('Command not found')
+    }
+
+    CliLog.info('[Command] Run')
     await PushbulletInstance.connect()
   })
 
@@ -46,10 +65,12 @@ cli.parse(process.argv, { run: false })
 const main = async () => {
   await bootstrap()
   await cli.runMatchedCommand()
-  CliLog.info('> Success')
 }
 
 main()
+  .then(res => {
+    CliLog.info('> Success')
+  })
   .catch(err => {
     CliLog.error(err)
     process.exit(1)
