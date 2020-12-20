ad1dfeccac823f76d2efebd57fd500b7889fc07b
mabasasi <aoe317293@yahoo.co.jp>
2020-12-20 18:59:22 +0900

    [fix] dm の type を修正

--- src/interface/twitter/DirectMessageInterface.ts
+++ src/interface/twitter/DirectMessageInterface.ts
@@ -1,17 +1,19 @@
 import TwitterEntityInterface from './TwitterEntityInterface'
 
 export default interface DirectMessageInterface {
-  type: 'message_create'
-  id: string // id_str と同一
-  created_timestamp: string // "1608358061004"
-  message_create?: {
-    target: {
-      recipient_id: string // 送信先 user id
-    }
-    sender_id: string // 送信元 user id
-    message_data: {
-      text: string
-      entities?: TwitterEntityInterface
+  event: {
+    type: 'message_create'
+    id: string // id_str と同一
+    created_timestamp: string // "1608358061004"
+    message_create: {
+      target: {
+        recipient_id: string // 送信先 user id
+      }
+      sender_id: string // 送信元 user id
+      message_data: {
+        text: string
+        entities?: TwitterEntityInterface
+      }
     }
   }
 }
